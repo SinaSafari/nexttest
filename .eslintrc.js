6defdbd5f593ae3cf9ec265dad2e8bce6fc8c584
sinasafari <m.sinasafari@gmail.com>
2021-09-27 09:58:55 +0330

    initial configuration

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  //   env: {
+  //     browser: true,
+  //     es2021: true,
+  //   },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "plugin:jest/recommended",
+    "plugin:testing-library/react",
+    "next/core-web-vitals",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  //   plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
